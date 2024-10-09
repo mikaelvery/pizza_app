788d7073dc4e062164c309774e86c55b5785f167
mikaelvery <mikael.very34@gmail.com>
2024-10-09 21:47:50 +0200

    add widget and maj pubspec

---
+++ lib/widget/delivery_tracking.dart
@@ -0,0 +1,20 @@
+// import 'package:flutter/material.dart';
+// import 'package:google_maps_flutter/google_maps_flutter.dart';
+// import 'package:pizzaapp/models/suivi_livraison/delivery_map.dart';
+
+// class DeliveryTrackingPage extends StatelessWidget {
+//   final LatLng deliveryLocation;
+
+//   const DeliveryTrackingPage({super.key, required this.deliveryLocation});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Suivi de la livraison'),
+//       ),
+//       body: DeliveryMap(deliveryLocation: deliveryLocation, pizzeriaLocation: const LatLng(43.659319, 3.912129)),
+//     );
+//   }
+// }
+
